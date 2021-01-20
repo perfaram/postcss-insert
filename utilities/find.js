a9b84e8b60ea2f3e38a4e6b5369525d143f7c195
Perceval Faramaz <perceval@faramaz.com>
2021-01-20 19:03:00 +0100

    Fix error logs: use actual project name

--- utilities/find.js
+++ utilities/find.js
@@ -5,16 +5,16 @@ module.exports = (toApply, lookup, options, onError) => {
   const items = _.get(lookup, toApply, []);
 
   if (_.isEmpty(items)) {
-    throw onError(`\`@inject\` No class named ${items}`);
+    throw onError(`\`@insert\` No class named ${items}`);
   } else if (items.length > 1) {
-    throw onError(`\`@inject\` Too many classes named ${items} not sure which to pick`);
+    throw onError(`\`@insert\` Too many classes named ${items} not sure which to pick`);
   }
 
   let [item] = items;
 
   if (! options.allowFromMediaQueries) {
     if (item.parent.type !== 'root') {
-      throw onError(`\`@inject\` cannot be used with ${toApply} because ${toApply} is nested inside of an at-rule (@${item.parent.name}).`)
+      throw onError(`\`@insert\` cannot be used with ${toApply} because ${toApply} is nested inside of an at-rule (@${item.parent.name}).`)
     }
   }
   else {//allowed
